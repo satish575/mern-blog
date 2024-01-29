281a9945df0edb9413f2fa85868db28576d00d39
[satish mishra] <[mishrasatish575@gmail.com]>
2024-01-29 21:53:52 +0545

    create signup api routes

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,17 @@
+import User from "../models/user.model.js"
+import bcryptjs from "bcryptjs";
+export const signup=async(req,res,next)=>{
+    try{
+    const {username,email,password}=req.body;
+
+    if(!username || !email || !password || username ==="" || email ==="" || password === ""){
+        return res.status(400).json({message:"All fields are required"});
+    }
+    const hashedPassword=bcryptjs.hashSync(password,10);
+    const newUser=new User({username,email,password:hashedPassword});
+    await newUser.save();
+    res.json("signup successful");
+}catch(err){
+   res.json(err.message);
+}
+}
--- api/index.js
+++ api/index.js
@@ -2,10 +2,13 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv"
 import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js"
 dotenv.config();
 
 const app=express();
+app.use(express.json());
 app.use("/api/user",userRoutes)
+app.use("/api/auth",authRoutes)
 mongoose.connect(process.env.MONGODB).then((respnse)=>{
    
 }).catch((error)=>{
